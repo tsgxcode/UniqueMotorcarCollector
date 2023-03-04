d74bc3946bb4fd50e07bb6707be6fd747eb00a57
Scott George <tsgcats@gmail.com>
2023-03-04 11:39:08 -0500

    Fixed some margins

--- Program.cs
+++ Program.cs
@@ -5,9 +5,11 @@
         string make = "Make";
         string modal = "Modal";
         string year = "Year";
-        double amountPaid = 0;
+        double amountPaid = 1.9;
 
 
         Console.WriteLine("Unique Motorcar Collector! Please add your latest purchas to the Database: ");
+
+        Console.WriteLine(make + ": Lamborghini  " + modal + ": Miura  " + year + ": 1967  " + "Paid " + "$" + amountPaid + "Million" );
     }
 }
