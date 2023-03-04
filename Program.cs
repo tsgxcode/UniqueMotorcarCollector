e1553f82c7779fb327bf56df0169f4cc47a56cdf
Scott George <tsgcats@gmail.com>
2023-03-04 11:18:43 -0500

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿internal class Program
+{
+   public  static void Main(string[] args)
+    {
+        string make = "Make";
+        string modal = "Modal";
+        string year = "Year";
+        double amountPaid = 0;
+
+
+        Console.WriteLine("Unique Motorcar Collector! Please add your latest purchas to the Database: ");
+    }
+}
