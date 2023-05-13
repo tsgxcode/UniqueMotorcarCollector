5da7345dbb1e16c87fc60e63157500f618d1691f
Scott George <tsgcats@gmail.com>
2023-05-13 11:21:35 -0400

    Changed project idea  to make it a restoration company for exotic/ unique cars.

--- Program.cs
+++ Program.cs
@@ -5,7 +5,7 @@ internal class Program
     public static void Main()
     {
         //Basic user input
-        Console.WriteLine("Unique Motorcar Collector! "
+        Console.WriteLine("Unique Motorcar Collector Restorations! "
             + "Please add your latest purchase to the Database: "
             + "");
         //Refactored: Import and store user input
@@ -20,6 +20,15 @@ internal class Program
         Console.Write("Year? ");
         var year = Console.ReadLine();
         Console.WriteLine(year);
+
+        Console.Write("Customer Info: ");
+        var cusInfo = Console.ReadLine();
+        Console.WriteLine(cusInfo);
+
+        //Send to teams for restoration stage
+
+
+
 
         // TO DOs:
         //Save to the database
