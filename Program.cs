4ea6affdf0e4a4ce121805ddfe37fd701e0c7fc8
Scott George <tsgcats@gmail.com>
2023-06-16 12:58:15 -0400

    User info gather, complete.

--- Program.cs
+++ Program.cs
@@ -1,16 +1,36 @@
 ﻿using System;
+using UniqueMotorcarCollector;
 
 internal class Program
 {
+
+    /* Send to one of three garages/teams depending on restoration step
+            to be determind by the service tech, for next restoration stage.
+            string garageTwo = "Garage 2 Paint?";
+            string garageThree = "Garage 3 Finish ?";
+
+
+
+
+            // TO DOs:
+            //Save to the database
+            {
+                //Next Step...
+
+            }*/
+
+
     public static void Main()
     {
         //Unique Motorcar Collector Restorations V. 1.01
-        Console.WriteLine("Welcome! Unique Motorcar Collector Restorations! " + "\n" );
 
-        Console.ForegroundColor = ConsoleColor. DarkYellow;
+        //Gather info
+        Console.WriteLine("Welcome! Unique Motorcar Collector Restorations! " + "\n");
+
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
         Console.WriteLine("Owner info: " + "\n");
         Console.ResetColor();
-        
+
         Console.Write("Name: ");
         var name = Console.ReadLine();
 
@@ -20,11 +40,12 @@ internal class Program
         Console.Write("Phone: ");
         var phone = Console.ReadLine();
 
+
         Console.Write("Email: ");
         var eMail = Console.ReadLine();
         Console.Write("\n");
 
-        //Add vehicle info
+
         Console.ForegroundColor = ConsoleColor.Yellow;
         Console.WriteLine("Vehicle info: ");
         Console.ResetColor();
@@ -56,16 +77,5 @@ internal class Program
         Console.Write("Deposit: ");
         var minDeposit = Console.ReadLine();
         Console.Write("\n");
-
-        //Send to garage teams for restoration stage
-
-
-
-        // TO DOs:
-        //Save to the database
-        {
-            //Next Step...
-
-        }
     }
 }
