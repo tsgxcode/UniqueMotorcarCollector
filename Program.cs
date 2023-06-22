b798b33241ecd93de25e223e7380f8d09bea4348
Scott George <tsgcats@gmail.com>
2023-06-21 22:19:55 -0400

    Refresher: adding try/catch loop to poject

--- Program.cs
+++ Program.cs
@@ -1,76 +1,106 @@
 ﻿using System;
+using System.Diagnostics;
 using UniqueMotorcarCollector;
+
 
 class Program
 {
 
-
-    /* 
-
-
-// TO DOs:
-//Save to the database
-{
-//Next Step...
-
-}*/
-
     public static void Main()
     {
-        //Unique Motorcar Collector Restorations V. 1.01
+        /*         //Unique Motorcar Collector Restorations V. 1.01
 
-        //Gather info
-        Console.WriteLine("Welcome! Unique Motorcar Collector Restorations! " + "\n");
+                //Gather info
+                Console.WriteLine("Welcome! Unique Motorcar Collector Restorations! " + "\n");
 
-        Console.ForegroundColor = ConsoleColor.DarkYellow;
-        Console.WriteLine("Owner info: " + "\n");
-        Console.ResetColor();
+                Console.ForegroundColor = ConsoleColor.DarkYellow;
+                Console.WriteLine("Owner info: " + "\n");
+                Console.ResetColor();
 
-        Console.Write("Name: ");
-        var name = Console.ReadLine();
+                Console.Write("Name: ");
+                var name = Console.ReadLine();
 
-        Console.Write("Address: ");
-        var address = Console.ReadLine();
-
-
-        Console.Write("Phone: ");
-        var phone = Console.ReadLine();
-
-        Console.Write("Email: ");
-        var eMail = Console.ReadLine();
-        Console.Write("\n");
+                Console.Write("Address: ");
+                var address = Console.ReadLine();
 
 
-        Console.ForegroundColor = ConsoleColor.Yellow;
-        Console.WriteLine("Vehicle info: ");
-        Console.ResetColor();
+                Console.Write("Phone: ");
+                var phone = Console.ReadLine();
 
-        Console.Write("Make: ");
-        var make = Console.ReadLine();
+                Console.Write("Email: ");
+                var eMail = Console.ReadLine();
+                Console.Write("\n");
 
-        Console.Write("Modal: ");
-        var modal = Console.ReadLine();
 
-        Console.Write("Year: ");
-        var year = Console.ReadLine();
-        Console.Write("\n");
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.WriteLine("Vehicle info: ");
+                Console.ResetColor();
 
-        Console.Write("Date submited to UMCR: " + "\n");
-        var subDate = Console.ReadLine();
-        Console.Write("\n");
+                Console.Write("Make: ");
+                var make = Console.ReadLine();
 
-        Console.ResetColor();
+                Console.Write("Modal: ");
+                var modal = Console.ReadLine();
 
-        Console.ForegroundColor = ConsoleColor.Magenta;
-        Console.WriteLine("Cost estimate and deposit. Note: Min. 20% required: " + "\n");
-        Console.ResetColor();
+                Console.Write("Year: ");
+                var year = Console.ReadLine();
+                Console.Write("\n");
 
-        Console.Write("Total cost estimate: ");
-        var costEst = Console.ReadLine();
-        Console.Write("\n");
+                Console.Write("Date submited to UMCR: " + "\n");
+                var subDate = Console.ReadLine();
+                Console.Write("\n");
 
-        Console.Write("Deposit: ");
-        var minDeposit = Console.ReadLine();
+                Console.ResetColor();
+
+                Console.ForegroundColor = ConsoleColor.Magenta;
+                Console.WriteLine("Cost estimate and deposit. Note: Min. 20% required: " + "\n");
+                Console.ResetColor();
+
+                Console.Write("Total cost estimate: ");
+                var costEst = Console.ReadLine();
+                Console.Write("\n");
+
+                Console.Write("Deposit: ");
+                var minDeposit = Console.ReadLine();
+                Console.Write("\n"); */
+
+        /* 
+        practice code
+        
+        int num = Convert.ToInt32("46");
+        Console.WriteLine(num + 8);
+ */
+        int year;
+        bool isValidInput = false;
+
+        while (!isValidInput)
+        {
+            try
+            {
+                Console.Write("Year: ");
+                var input = Console.ReadLine();
+
+                if (int.TryParse(input, out year))
+                {
+                    // Valid input, exit the loop
+                    isValidInput = true;
+                }
+                else
+                {
+                    // Invalid input, display an error message and continue the loop
+                    Console.WriteLine("Invalid input. Please enter a valid number.");
+                }
+            }
+            catch (Exception ex)
+            {
+                // Exception handling code goes here
+                Console.WriteLine($"An error occurred: {ex.Message}");
+            }
+        }
+
+        // Use the valid 'year' input for further processing
+        //Console.WriteLine($"Year: {""}");
+
         Console.Write("\n");
     }
 }
