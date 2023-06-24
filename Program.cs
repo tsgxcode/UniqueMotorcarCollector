1bfaf45e38dda9459ed62a93b7cea43c3425f624
Scott George <tsgcats@gmail.com>
2023-06-23 20:18:50 -0400

    Adding Exception handling.

--- Program.cs
+++ Program.cs
@@ -8,68 +8,40 @@ class Program
 
     public static void Main()
     {
-        /*         //Unique Motorcar Collector Restorations V. 1.01
+        //Unique Motorcar Collector Restorations V. 1.01
 
-                //Gather info
-                Console.WriteLine("Welcome! Unique Motorcar Collector Restorations! " + "\n");
+        //Gather info
+        Console.WriteLine("Welcome! Unique Motorcar Collector Restorations! " + "\n");
 
-                Console.ForegroundColor = ConsoleColor.DarkYellow;
-                Console.WriteLine("Owner info: " + "\n");
-                Console.ResetColor();
+        Console.ForegroundColor = ConsoleColor.DarkYellow;
+        Console.WriteLine("Owner info: " + "\n");
+        Console.ResetColor();
 
-                Console.Write("Name: ");
-                var name = Console.ReadLine();
+        Console.Write("Name: ");
+        var name = Console.ReadLine();
 
-                Console.Write("Address: ");
-                var address = Console.ReadLine();
-
-
-                Console.Write("Phone: ");
-                var phone = Console.ReadLine();
-
-                Console.Write("Email: ");
-                var eMail = Console.ReadLine();
-                Console.Write("\n");
+        Console.Write("Address: ");
+        var address = Console.ReadLine();
 
 
-                Console.ForegroundColor = ConsoleColor.Yellow;
-                Console.WriteLine("Vehicle info: ");
-                Console.ResetColor();
+        Console.Write("Phone: ");
+        var phone = Console.ReadLine();
 
-                Console.Write("Make: ");
-                var make = Console.ReadLine();
+        Console.Write("Email: ");
+        var eMail = Console.ReadLine();
+        Console.Write("\n");
 
-                Console.Write("Modal: ");
-                var modal = Console.ReadLine();
 
-                Console.Write("Year: ");
-                var year = Console.ReadLine();
-                Console.Write("\n");
+        Console.ForegroundColor = ConsoleColor.Yellow;
+        Console.WriteLine("Vehicle info: ");
+        Console.ResetColor();
 
-                Console.Write("Date submited to UMCR: " + "\n");
-                var subDate = Console.ReadLine();
-                Console.Write("\n");
+        Console.Write("Make: ");
+        var make = Console.ReadLine();
 
-                Console.ResetColor();
+        Console.Write("Modal: ");
+        var modal = Console.ReadLine();
 
-                Console.ForegroundColor = ConsoleColor.Magenta;
-                Console.WriteLine("Cost estimate and deposit. Note: Min. 20% required: " + "\n");
-                Console.ResetColor();
-
-                Console.Write("Total cost estimate: ");
-                var costEst = Console.ReadLine();
-                Console.Write("\n");
-
-                Console.Write("Deposit: ");
-                var minDeposit = Console.ReadLine();
-                Console.Write("\n"); */
-
-        /* 
-        practice code
-        
-        int num = Convert.ToInt32("46");
-        Console.WriteLine(num + 8);
- */
         int year;
         bool isValidInput = false;
 
@@ -96,7 +68,34 @@ class Program
                 // Exception handling code goes here
                 Console.WriteLine($"An error occurred: {{}}");
             }
-        }
+        };
+        Console.Write("\n");
+
+        Console.Write("Date submited to UMCR: " + "\n");
+        var subDate = Console.ReadLine();
+        Console.Write("\n");
+
+        Console.ResetColor();
+
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        Console.WriteLine("Cost estimate and deposit. Note: Min. 20% required: " + "\n");
+        Console.ResetColor();
+
+        Console.Write("Total cost estimate: ");
+        var costEst = Console.ReadLine();
+        Console.Write("\n");
+
+        Console.Write("Deposit: ");
+        var minDeposit = Console.ReadLine();
+        Console.Write("\n");
+
+        /* 
+        practice code
+        
+        int num = Convert.ToInt32("46");
+        Console.WriteLine(num + 8);
+ */
+
 
         Console.Write("\n");
     }
